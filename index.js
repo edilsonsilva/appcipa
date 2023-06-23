0989d8f20e0d374e96f1b5c0f5c44cb6d6074140
Edilson Silva <edilson_js@yahoo.com.br>
2023-06-23 11:00:54 -0300

    Modificacao da rota

--- index.js
+++ index.js
@@ -6,31 +6,32 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-
-const conexao = mysql.createConnection({
-    host:"127.0.0.1",
-    user:"root",
-    password:"Alunos@123",
-    database:"cipadb",
-    port:"6556"
+app.get("/teste", (req, res) => {
+  res.status(200).send({ output: "Tudo funcionando" });
 });
 
-conexao.connect((error)=>{
-    if(error) return console.error(`Erro ao estabelecer a conexão com o banco -> ${error}`)
-    else console.log(`Conexão com o banco estabelecida -> ${conexao.threadId}`)
-})
+// const conexao = mysql.createConnection({
+//     host:"127.0.0.1",
+//     user:"root",
+//     password:"Alunos@123",
+//     database:"cipadb",
+//     port:"6556"
+// });
 
+// conexao.connect((error)=>{
+//     if(error) return console.error(`Erro ao estabelecer a conexão com o banco -> ${error}`)
+//     else console.log(`Conexão com o banco estabelecida -> ${conexao.threadId}`)
+// })
 
-app.get("/noticia/listar",(req,res)=>{
-    conexao.query("Select * from noticia order by idnoticia desc",(error,result)=>{
-        if(!error){
-            res.status(200).send({output:"Ok",payload:result})
-        }
-        else{
-            return res.status(500).send({output:"Erro",erro:error})
-        }
-    });
-});
+// app.get("/noticia/listar",(req,res)=>{
+//     conexao.query("Select * from noticia order by idnoticia desc",(error,result)=>{
+//         if(!error){
+//             res.status(200).send({output:"Ok",payload:result})
+//         }
+//         else{
+//             return res.status(500).send({output:"Erro",erro:error})
+//         }
+//     });
+// });
 
-
-app.listen(4000,()=>console.log(`Servidor online em http://127.0.0.1:4000`));
+// app.listen(4000,()=>console.log(`Servidor online em http://127.0.0.1:4000`));
